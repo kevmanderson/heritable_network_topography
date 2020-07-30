73a7d6c841ff52f2a894cd3b7e793645b5c3015d
KevAnder <kevin.anderson@yale.edu>
2020-07-30 14:18:31 -0400

    upload

--- scripts/08_net_topograpy_h2_wrapper.m
+++ scripts/08_net_topograpy_h2_wrapper.m
@@ -57,7 +57,7 @@ end
 
 
 
-% example using height phenotype
+% example/double-check using height phenotype
 
 pheno = csvread(fullfile(base_dir, 'Height_P.csv'), 0);
 covar = csvread(fullfile(base_dir, 'Height_covar.csv'), 1);
@@ -65,67 +65,7 @@ covar = csvread(fullfile(base_dir, 'Height_covar.csv'), 1);
 [h2, p_perm] = h2_mat(pheno, kin, covar, 10);
 
 
-out_mat = {};
-pheno = csvread(fullfile(base_dir, 'dice_net_12_P.csv'), 0);
-[h2, p_perm] = h2_mat(pheno, kin, covar, 1000);
-cur_row = [h2, p_perm];
 
-
-
-
-base_dir = '/gpfs/milgram/scratch/holmes/topo_herit/data/topology_heritability'
-base_dir = '/gpfs/milgram/project/holmes/kma52/topo_herit/data/topology_heritability/'
-% HCP kinship matrix
-kin   = csvread(fullfile(base_dir, 'K.csv'), 1);
-
-% HCP covariates
-covar = csvread(fullfile(base_dir, 'covar.csv'), 1);
-
-% Family IDs
-fam = csvread(fullfile(base_dir, 'F.csv'), 1);
-
-% calculate heritability of network topography
-out_mat = {};
-hemi_arr = {'lh','rh','bihemi'};
-for (i = 1:3)
-    hemi         = hemi_arr{i};
-    hemi
-
-    out_mat      = table();
-    pheno        = csvread(fullfile(base_dir, [hemi '_overall_dice_P.csv']), 0);
-
-    K=kin; P=pheno; X=covar;
-    %[h2, p_perm] = h2_mat(pheno, kin, covar, fam, 1000);
-    [h2, p_perm, jack_se] = h2_mat(pheno, kin, covar, 10, fam);
-    out_mat(1,:) = {h2, p_perm, jack_se, 'overall', hemi};
-
-    for (i = 1:17)
-        disp(i)
-        pheno = csvread(fullfile(base_dir, [hemi '_dice_net_', num2str(i),'_P.csv']), 0);
-        [h2, p_perm]   = h2_mat(pheno, kin, covar, 1000);
-        out_mat(i+1,:) = {h2, p_perm, num2str(i), hemi};
-    end
-
-    out_path = fullfile(base_dir, [hemi '_dice_network_topology_h2.csv']);
-    writetable(out_mat, out_path)
-end
-
-
-
-% HCP kinship matrix
-K = csvread('K.csv', 1);
-
-% HCP covariates
-X = csvread('covar.csv', 1);
-
-% Family IDs
-F = csvread('F.csv', 1);
-
-% Dice similarity
-P = csvread('lh_overall_dice_P.csv', 1);
-
-% calc h2
-[h2, p_perm] = h2_mat(P, K, X, F, 1000);
 
 
 
